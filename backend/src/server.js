dfbf3310bafebf7c196cc04f7d9b0a1b4d6d475f
mandinumaneth <wamandinumaneth@gmail.com>
2025-08-17 16:09:17 +0530

    completed 1

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,25 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
-import rateLimiter from "./middleware/reteLimiter.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-
+//middleware
+app.use(cors({
+  origin: "http://localhost:5173", // Replace with your frontend URL
+}));
 app.use(express.json()); // Middleware to parse JSON bodies : req.body
-
 app.use(rateLimiter); // Apply rate limiting middleware
 
-app.use("/api/notes", notesRoutes);
+
+app.use("/api/notes", notesRoutes); // Apply notes routes
 
 connectDB().then(() => {
   app.listen(PORT, () => {
