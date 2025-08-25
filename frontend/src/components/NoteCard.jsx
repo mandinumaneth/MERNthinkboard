053e69e62af202db9848e4036be6ed409bb56183
mandinumaneth <wamandinumaneth@gmail.com>
2025-08-25 19:05:24 +0530

    Updated Note card

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -23,16 +23,20 @@ const NoteCard = ({ note, setNotes }) => {
   return (
     <Link
       to={`/note/${note._id}`}
-      className="card bg-base-100 hover:shadow-lg transition-all duration-200 
-      border-t-4 border-solid border-[#00FF9D]"
+      className="card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid border-[#00FF9D]"
     >
       <div className="card-body">
         <h3 className="card-title text-base-content">{note.title}</h3>
         <p className="text-base-content/70 line-clamp-3">{note.content}</p>
         <div className="card-actions justify-between items-center mt-4">
-          <span className="text-sm text-base-content/60">
-            {formatDate(new Date(note.createdAt))}
-          </span>
+          <div>
+            <span className="text-sm text-base-content/60 block">
+              Created: {formatDate(new Date(note.createdAt))}
+            </span>
+            <span className="text-sm text-base-content/60 block">
+              Updated: {formatDate(new Date(note.updatedAt))}
+            </span>
+          </div>
           <div className="flex items-center gap-1">
             <PenSquareIcon className="size-4" />
             <button
